12e08b86aa6337d1004009d2df702e863c105ab2
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-09-01 20:36:33 -0400

    Add logic to determine correctness and increase score.

--- ally.js
+++ ally.js
@@ -6,3 +6,38 @@ let quiz2 = prompt("Whose murder in Sanford, FL sparked a national protest?");
 let quiz3 = prompt("Whose death was the catalyst for the BLM movement?");
 let quiz4 = prompt("Who was murdered by a police officer on the BART train station?");
 let quiz5 = prompt("Who was murdered by an officer after having his neck kneeled upon for 8:46?");
+
+if (quiz1.toUpperCase() === "ERIC GARNER") {
+  // console.log("That's right!");
+  correct += 1;
+} else {
+  console.log("That is incorrect.");
+}
+
+if (quiz2.toUpperCase() === "TRAYVON MARTIN") {
+  // console.log("That's right!");
+  correct += 1;
+} else {
+  console.log("That is incorrect.");
+}
+
+if (quiz3.toUpperCase() === "MICHAEL BROWN") {
+  // console.log("That's right!");
+  correct += 1;
+} else {
+  console.log("That is incorrect.");
+}
+
+if (quiz4.toUpperCase() === "OSCAR GRANT") {
+  // console.log("That's right!");
+  correct += 1;
+} else {
+  console.log("That is incorrect.");
+}
+
+if (quiz5.toUpperCase() === "GEORGE FLOYD") {
+  // console.log("That's right!");
+  correct += 1;
+} else {
+  console.log("That is incorrect.");
+}
