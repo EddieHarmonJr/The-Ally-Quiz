ed16e745f3f31f9ceada7895bc3e1d5201362b20
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-09-01 20:31:21 -0400

    Add initial quiz questions.

--- ally.js
+++ ally.js
@@ -1,3 +1,8 @@
 let correct = 0;
 let playerOneRank = "";
 
+let quiz1 = prompt("Whose last words were 'I can\'t breathe'?");
+let quiz2 = prompt("Whose murder in Sanford, FL sparked a national protest?");
+let quiz3 = prompt("Whose death was the catalyst for the BLM movement?");
+let quiz4 = prompt("Who was murdered by a police officer on the BART train station?");
+let quiz5 = prompt("Who was murdered by an officer after having his neck kneeled upon for 8:46?");
