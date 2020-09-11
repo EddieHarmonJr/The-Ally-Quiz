a9709c96398f479c354e46f24e31cac6fcff5512
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-09-10 21:37:06 -0400

    Reformat quiz questions to be stored inside a data structure.

--- ally.js
+++ ally.js
@@ -1,11 +1,20 @@
 let correct = 0;
 let playerOneRank = "";
 
-let quiz1 = prompt("Whose last words were 'I can\'t breathe'?");
-let quiz2 = prompt("Whose murder in Sanford, FL sparked a national protest?");
-let quiz3 = prompt("Whose death was the catalyst for the BLM movement?");
-let quiz4 = prompt("Who was murdered by a police officer on the BART train station?");
-let quiz5 = prompt("Who was murdered by an officer after having his neck kneeled upon for 8:46?");
+// let quiz1 = prompt("Whose last words were 'I can\'t breathe'?");
+// let quiz2 = prompt("Whose murder in Sanford, FL sparked a national protest?");
+// let quiz3 = prompt("Whose death was the catalyst for the BLM movement?");
+// let quiz4 = prompt("Who was murdered by a police officer on the BART train station?");
+// let quiz5 = prompt("Who was murdered by an officer after having his neck kneeled upon for 8:46?");
+
+
+let quiz = [
+  ["Whose last words were 'I can\'t breathe'?", "ERIC GARNER"],
+  ["Whose murder in Sanford, FL sparked a national protest?", "TRAYVON MARTIN"],
+  ["Whose death was the catalyst for the BLM movement?", "MICHAEL BROWN"],
+  ["Who was murdered by a police officer on the BART train station?", "OSCAR GRANT"],
+  ["Who was murdered by an officer after having his neck kneeled upon for 8:46?", "GEORGE FLOYD"],
+];
 
 if (quiz1.toUpperCase() === "ERIC GARNER") {
   // console.log("That's right!");
