8bd17f848f1e4e245581b4d91b5e5e98b5617133
EddieHarmonJr <eddieharmon@ac.c4q.nyc>
2020-12-15 14:30:21 -0500

    add node.js article for reference material.

--- ally.js
+++ ally.js
@@ -2,34 +2,33 @@ let correct = 0;
 let wrong = 0;
 let playerOneRank = "";
 
-
 let quiz = [
   {question: "Whose last words were 'I can\'t breathe'?", answer: "ERIC GARNER"},
   {question: "Whose murder in Sanford, FL sparked a national protest?", answer: "TRAYVON MARTIN"},
   {question: "Whose death was the catalyst for the BLM movement?", answer: "MICHAEL BROWN"},
   {question: "Who was murdered by a police officer on the BART train station?", answer: "OSCAR GRANT"},
   {question: "Who was murdered by an officer after having his neck kneeled upon for 8:46?", answer: "GEORGE FLOYD"},
-  {question: "Who died in police custody after being pulled over for an alleged failure to signal?", answer: "SANDRA BLAND"},
-  {question: "Who was shot and killed by police while playing video games in the house with their nephew?", answer: "ATATIANNA JEFFERSON"},
-  {question: "Who was shot 6 times by police while sleeping in their apartment?", answer: "BREONNA TAYLOR"},
-  {question: "Who was killed by police in 2019 while walking home when someone called 911, saying he 'looked sketchy'?", answer: "ELIJAH MCCLAIN"},
-  {question: "What child was killed by police for playing with a toy gun in the park?", answer: "TAMIR RICE"},
-  {question: "Who was killed while in police custody and abused so badly that their spinal cord was severed?", answer: "FREDDIE GRAY"},
-  {question: "Who was killed by a police officer in Sacramento, CA when the officer alledged that the victum had a gun, when in fact, it was a cell phone?", answer: "STEPHON CLARK"},
-  {question: "Who was the person who was killed by a police officer while eating ice cream in his own apartment?", answer: "BOTHAM JEAN"},
-  {question: "After informing a police officer in Minnesota that he was carrying a legal firearm, this person was shot and killed while sitting in the car with his girlfriend and child.", answer: "PHILANDO CASTILLE"},
-  {question: "Who was shot six times after being confronted by police officers for selling CD's and DVD's in Baton Rouge, LA?", answer: "ALTON STERLING"},
-  {question: "Who was shot 16 times in Chicago for alledgedly approaching a police officer with a knife?", answer: "LAQUAN MCDONALD"},
-  {question: "Who was killed by police while in bed next to his 9 month pregnant fiancé after 4am?", answer: "FRED HAMPTON"},
-  {question: "Who was shot in the back of the head by a police officer while riding unarmed in the front passenger's seat of a friend's car at 15 years old?", answer: "JORDAN EDWARDS"},
-  {question: "What unarmed person was shot and killed in September 2016 in Tulsa, Oklahoma while standing next to his vehicle in the middle of the road?", answer: "TERENCE CRUTCHER"},
-  {question: "Who was shot and killed by police on the morning before his wedding in NYC?", answer: "SEAN BELL"},
-  {question: "Who was killed by New York City police officers when they fired 41 shots, (he was struck 19 times) at the age of 23?", answer: "AMADOU DIALLO"},
-  {question: "What individual was shot and killed by police because they fell asleep in a Wendy's drive-thru?", answer: "RAYSHARD BROOKS"},
-  {question: "Who was killed when his brother called for help, because his brother was having a mental health episode?", answer: "DANIEL PRUDE"},
-  {question: "Who was killed by Baltimore County police in 2016 after a 6-hour standoff, where she was shot 5 times?", answer: "KORRYN GAINES"},
-  {question: "What 22 year old was shot and killed by a police officer while inside a Walmart, after a false report that the victim waved a gun at patrons in the store?", answer: "JOHN CRAWFORD"},
-  {question: "What 22-year-old Black American was fatally shot as a bystander in Chicago, Illinois by an off-duty Chicago police detective, on March 21, 2012?", answer: "REKIA BOYD"}
+  {question: "Who died in police custody after being pulled over for an alleged failure to signal?", answer: "SANDRA BLAND"}
+  // {question: "Who was shot and killed by police while playing video games in the house with their nephew?", answer: "ATATIANNA JEFFERSON"},
+  // {question: "Who was shot 6 times by police while sleeping in their apartment?", answer: "BREONNA TAYLOR"},
+  // {question: "Who was killed by police in 2019 while walking home when someone called 911, saying he 'looked sketchy'?", answer: "ELIJAH MCCLAIN"},
+  // {question: "What child was killed by police for playing with a toy gun in the park?", answer: "TAMIR RICE"},
+  // {question: "Who was killed while in police custody and abused so badly that their spinal cord was severed?", answer: "FREDDIE GRAY"},
+  // {question: "Who was killed by a police officer in Sacramento, CA when the officer alledged that the victum had a gun, when in fact, it was a cell phone?", answer: "STEPHON CLARK"},
+  // {question: "Who was the person who was killed by a police officer while eating ice cream in his own apartment?", answer: "BOTHAM JEAN"},
+  // {question: "After informing a police officer in Minnesota that he was carrying a legal firearm, this person was shot and killed while sitting in the car with his girlfriend and child.", answer: "PHILANDO CASTILLE"},
+  // {question: "Who was shot six times after being confronted by police officers for selling CD's and DVD's in Baton Rouge, LA?", answer: "ALTON STERLING"},
+  // {question: "Who was shot 16 times in Chicago for alledgedly approaching a police officer with a knife?", answer: "LAQUAN MCDONALD"},
+  // {question: "Who was killed by police while in bed next to his 9 month pregnant fiancé after 4am?", answer: "FRED HAMPTON"},
+  // {question: "Who was shot in the back of the head by a police officer while riding unarmed in the front passenger's seat of a friend's car at 15 years old?", answer: "JORDAN EDWARDS"},
+  // {question: "What unarmed person was shot and killed in September 2016 in Tulsa, Oklahoma while standing next to his vehicle in the middle of the road?", answer: "TERENCE CRUTCHER"},
+  // {question: "Who was shot and killed by police on the morning before his wedding in NYC?", answer: "SEAN BELL"},
+  // {question: "Who was killed by New York City police officers when they fired 41 shots, (he was struck 19 times) at the age of 23?", answer: "AMADOU DIALLO"},
+  // {question: "What individual was shot and killed by police because they fell asleep in a Wendy's drive-thru?", answer: "RAYSHARD BROOKS"},
+  // {question: "Who was killed when his brother called for help, because his brother was having a mental health episode?", answer: "DANIEL PRUDE"},
+  // {question: "Who was killed by Baltimore County police in 2016 after a 6-hour standoff, where she was shot 5 times?", answer: "KORRYN GAINES"},
+  // {question: "What 22 year old was shot and killed by a police officer while inside a Walmart, after a false report that the victim waved a gun at patrons in the store?", answer: "JOHN CRAWFORD"},
+  // {question: "What 22-year-old Black American was fatally shot as a bystander in Chicago, Illinois by an off-duty Chicago police detective, on March 21, 2012?", answer: "REKIA BOYD"}
 ];
 
 let numOfQuestions = quiz.length;
@@ -41,8 +40,7 @@ const calculateScore = (totalCorrect, totalQuestions) => {
   return finalScore;
 };
 
-// calculateScore(correct, numOfQuestions);
-// Your score was: ${Math.round(calculateScore(correct, numOfQuestions))}
+calculateScore(correct, numOfQuestions);
 //create logic to iterate through questions in the keys.
 
 for (let i = 0; i < quiz.length; i++) {
@@ -53,6 +51,7 @@ for (let i = 0; i < quiz.length; i++) {
     wrong += 1;
     //console log for checking accuracy of answerd questions:
     console.log(`Sorry. The correct answer to the question "${quiz[i].question}" is: ${quiz[i].answer}. You submitted: ${response}`);
+    console.log(calculateScore(correct, numOfQuestions));
   }
 }
 
@@ -82,7 +81,7 @@ if (calculateScore === 100) {
 
 let message = `<h1>You got ${correct} out of ${numOfQuestions} questions correct. You got ${wrong} questions incorrect.</h1> 
 
-<h1>Crown earned:  ${playerOneRank}.</h1>`;
+<h1>Crown earned: ${playerOneRank}.</h1>`;
 
 document.querySelector('h2').innerHTML = message;
 
@@ -108,6 +107,8 @@ document.querySelector('h2').innerHTML = message;
 //https://www.reneeater.com/on-monuments-blog/tag/list+of+unarmed+black+people+killed+by+police
 
 //https://www.nytimes.com/article/who-was-elijah-mcclain.html
+
+//https://nodejs.org/en/black-lives-matter/
 
 // End of Articles...
 
